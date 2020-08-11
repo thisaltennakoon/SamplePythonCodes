f5b7d8dee51ab9f74d92c4122dc019ab44c23d42
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2020-08-11 16:54:03 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,23 @@
-def binary_search(array, item,mid1=0):
-    mid = len(array)//2
-    mid1 = mid1 + len(array)//2
-    if array[mid]==item:
-        return mid1
-    elif array[mid]>item:
-        return binary_search(array[0:mid], item)
+def binary_search(array,item,index=0):
+    array_size=len(array)
+    if array_size==0:
+        return -1
+    elif array_size==1 :
+        if array[0]==item:
+            return 0
+        else:
+            return -1
     else:
-        return binary_search(array[mid:],item,mid1)
-
-a=[1,2,3,4,5,6]
-print(binary_search(a,1))
-print(binary_search(a,2))
-print(binary_search(a,3))
-print(binary_search(a,4))
-print(binary_search(a,5))
-print(binary_search(a,6))
+        mid=array_size//2
+        index=index+mid
+        if array[mid]==item:
+            return index
+        elif array[mid]>item:
+            return f(array[0:mid],item)
+        else:
+            return f(array[mid:], item, index)
+a=[1,2,3,4,5,6,7,8,9]
+print(f(a,0))
+print(f(a,1))
+print(f(a,2))
+print(f(a,9))
